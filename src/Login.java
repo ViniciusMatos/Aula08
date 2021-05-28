0afaab1784658cf22fbed40951fe7311d21c47e1
Vinicius Matos <vmatos12@gmail.com>
2021-05-28 20:11:49 -0300

    Criada classe Juros

--- src/Login.java
+++ src/Login.java
@@ -1,19 +1,20 @@
 
 public class Login {
 	public static void main(String args[]) {
-		String n = "admin@fiap.com.br";
-		String s = "12345";
+		String emailDigitado = "admin@fiap.com.br";
+		String senhaDigitada = "12345";
+		String tokenDigitado = "beatriz";
 		
-		boolean loginOk = validarLogin(n,s);
+		boolean loginOk = validarLogin(emailDigitado,tokenDigitado,senhaDigitada);
 		
 		System.out.println(loginOk);
 		
 	}
 	
-	public static boolean validarLogin(String email, String senha) {
+	public static boolean validarLogin(String email,String token, String senha) {
 		boolean retorno = false;
 		
-		if(email.equals("admin@fiap.com.br") && senha.equals("123456")) {
+		if(email.equals("admin@fiap.com.br") && senha.equals("123456") && token.equals("beatriz")) {
 			retorno = true;
 		}
 		return true;
